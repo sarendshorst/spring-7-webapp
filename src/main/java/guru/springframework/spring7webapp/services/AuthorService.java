0e4016333e59c1e0910ef61530dc7be42391e4d1
Stefan Arendshorst <stefan.arendshorst@soprasteria.com>
2026-04-29 15:22:08 +0200

    Added authors

---
+++ src/main/java/guru/springframework/spring7webapp/services/AuthorService.java
@@ -0,0 +1,9 @@
+package guru.springframework.spring7webapp.services;
+
+import guru.springframework.spring7webapp.domain.Author;
+
+public interface AuthorService {
+
+    Iterable<Author> findAll();
+
+}
